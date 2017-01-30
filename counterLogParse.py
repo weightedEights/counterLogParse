623334c709fd725fed832aa7a39a94bc4d3ca042
weightedEights <poormanjosh@gmail.com>
2017-01-30 15:28:27 -0300

    working with getopt module

--- counterLogParse.py
+++ counterLogParse.py
@@ -5,7 +5,13 @@ from __future__ import print_function
 
 """
 
-Summary
+The purpose is to read one or more log files, concatenate their data into one new file, and to generate a simple trend
+plot (freq vs time).
+
+This script is also an exercise in parsing command line switches. For example, choosing to only concatenate logs without
+generating a plot. Or, generating a plot based on one log and not concatenating a range.
+
+Usage = counterLogParse.py [-h] [-p] [-r] <logFileRangeStart> <logFileRangeEnd>
 
 """
 
@@ -21,10 +27,27 @@ def main(argv):
     cattedDatArray = loadLogs("test.data.txt")
     writeCattedFile(cattedFile)
 
-    if len(sys.argv) > 1 and sys.argv[1] == '-s' and sys.argv[2] == True:
-        funcVal = secFunc(funcVal, sys.argv[2])
-
     plotCattedTrend(cattedDatArray)
+
+    inputFile = ''
+    outputFile = ''
+    try:
+        opts, args = getopt.getopt(argv, "hi:o:", ["inFile=", "outFile="])
+
+    except getopt.GetoptError:
+        print("USAGE")
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt == '-h':
+            print("USAGE")
+            sys.exit()
+
+        elif opt in ("-i", "--inFile"):
+            inputFile = arg
+
+        elif opt in ("-o", "--outFile"):
+            outputFile = arg
 
 
 def cattedFileSetup(name, path):
