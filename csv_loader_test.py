9022dbf6253c7d9c10b8101016f951158448acf8
weightedEights <poormanjosh@gmail.com>
2018-01-16 17:53:32 -0300

    appending the dict to the running_frame works on command line with simple data, does not work as written

--- csv_loader_test.py
+++ csv_loader_test.py
@@ -12,36 +12,47 @@ Load, parse, and plot a single .csv file of counter logger data.
 """
 
 
-import os
+# import os
+# from datetime import datetime
 import glob
 import pandas as pd
-from matplotlib import pyplot as plt
-import seaborn as sns
-import numpy as np
+# from matplotlib import pyplot as plt
+# import seaborn as sns
+# import numpy as np
 
 
 def main():
 
     pd.options.display.float_format = "{:.7f}".format
+    # pd.set_option('precision', 7)
 
-    # create empty data frame to populate
-    new_frame = pd.DataFrame()
+    # create empty data frame to populate, for future this will be a new file to write daily data points
+    # csv write format: call_df.to_csv('D:/Call.csv', date_format='%Y-%m-%d %H:%M:%S')
+    running_frame = pd.DataFrame(columns=['Date', 'CounterAvg'])
 
     file_list = [x for x in glob.glob("testLog.2016[12]*")]
 
     for f in file_list:
         print("Working on file: {}...".format(f))
-        working_frame = pd.read_csv(f, skiprows=0, parse_dates=['Time'])
-        # print(working_frame)
-        new_frame = new_frame.append(working_frame)
-        # print(new_frame)
 
-    print(new_frame)
+        # sep= resolves the issue of spaces in the column names when importing
+        working_frame = pd.read_csv(f, sep='\s*,\s*', skiprows=0, parse_dates=['Time'], engine='python')
+        print("Working frame: \n{}\n".format(working_frame))
 
-    # create the unordered set first, then convert to list.ordered()
-    the_days = set(d.day for d in new_frame["Time"])
+        # get date from frame
+        # get mean from one day of measurements
+        working_date = working_frame['Time'][0].date()
+        working_mean = working_frame['CounterData'].mean()
+        print("Date: {}, Avg: {:.7f}\n".format(working_date, working_mean))
 
-    print(the_days)
+        # create new row to concat to running_frame using standard dict format
+        working_dict = {"Date": working_date, "CounterAvg": working_mean}
+        print("Working dict: {}\n".format(working_dict))
+
+        # append to running frame
+        running_frame.append(working_dict, ignore_index=True)
+        print("Running frame: \n{}\n".format(running_frame))
+
 
 if __name__ == '__main__':
     main()
